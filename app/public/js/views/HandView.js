eb1d7ecf5172cc1088a927dcdb7294dd29a2452a
June Jung <jeongjueun@gmail.com>
2013-02-10 16:18:05 -0800

    moved all the tmp branch change

--- app/public/js/views/HandView.js
+++ app/public/js/views/HandView.js
@@ -1,1 +1,39 @@
-//Backbone
+define(function(require) {
+  var $        = require('jquery'),
+      Backbone = require('backbone');
+
+  var HandView = Backbone.View.extend({
+
+    initialize: function(){
+
+    },
+
+    events:{
+      'click' : function(e) {
+        // not sure if 'this' is pointing to right reference
+        this.placePiece(e.target.id);
+      }
+    },
+
+    tileStack: {
+      Queen : 1,
+      Spider : 2,
+      Beetle : 2,
+      Ant : 3,
+      Grasshopper: 3
+    },
+
+    placePiece: function(type){
+      this.tileStack[type] = this.tileStack[type] - 1;
+      this.collection.add({
+        model: new Queen(),
+        //gameId: length of collection
+      });
+      if (this.tileStack[type] === 0){
+        //make it unclickable
+      }
+    }
+  });
+
+  return HandView;
+});
--- app/public/js/views/StageView.js
+++ app/public/js/views/StageView.js
@@ -1,1 +1,36 @@
-//Backbone
+define(function(require) {
+  var $        = require('jquery'),
+      Backbone = require('backbone'),
+      Kinetic  = require('kinetic');
+
+
+  var StageView = Backbone.View.extend({
+
+    initialize: function(){
+      this.collection.bind("change", this.render, this);
+      this.stage = new Kinetic.Stage({
+        container: 'container',
+        width: 1400,
+        height: 600,
+        draggable: true
+      });
+      this.layer =  new Kinetic.Layer();
+    },
+
+    render: function(){
+      // TODO: clear out the layer before
+      var self = this;
+      _.each(self.collection, function(piece){
+        var hexView = new PieceView({
+          model: piece,
+          hex : new Kinetic.RegularPolygon(piece.hexStyle)
+        });
+        self.layer.add(hexView.hex);
+      });
+      this.layer.draw();
+    }
+
+  });
+
+  return StageView;
+});
